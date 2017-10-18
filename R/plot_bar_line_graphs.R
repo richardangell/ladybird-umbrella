48e71c7edb48f024fd9606dad3c88ad1025e0260
richardangell <richardangell37@gmail.com>
2017-10-18 18:47:47 +0100

    turn plotting into function; weights and observed only

--- R/plot_bar_line_graphs.R
+++ R/plot_bar_line_graphs.R
@@ -1,5 +1,6 @@
 library(plotly)
 library(dplyr)
+library(rlang)
 
 data <- data.frame(a = runif(100),
                    b = rnorm(100),
@@ -18,23 +19,59 @@ xx <- summarise_column(df = data,
                        weights = "c")
 
 
-p <- plot_ly(xx$summary) %>%
-  add_trace(x = ~binned_ordered, y = ~c, type = 'bar', name = 'c',
-            marker = list(color = 'yellow'),
-            hoverinfo = "text",
-            text = ~paste(c, ' (total weight)')) %>%
-  add_trace(x = ~binned_ordered, y = ~b, type = 'scatter', mode = 'lines', name = 'b', yaxis = 'y2',
-            line = list(color = 'pink'),
-            hoverinfo = "text",
-            text = ~paste(b, 'ave observed')) %>%
-  layout(title = 'variable: a',
-         xaxis = list(title = ""),
-         yaxis = list(side = 'right', title = 'total weight', showgrid = F, zeroline = F),
-         yaxis2 = list(side = 'left', overlaying = "y", title = 'average observed', showgrid = F, zeroline = F))
+plot_bar_line_graph(df = xx$summary,
+                    col = "binned_ordered",
+                    weight = "c",
+                    observed = "b")
 
 
-p
-
+plot_bar_line_graph <- function(df,
+                                col,
+                                weight,
+                                observed,
+                                rounding_digits = 5) {
+  
+  # need to find a better way to set the width so it looks good in the nb.html
+  p <- plot_ly(df, width = 900) %>%
+    
+    add_trace(x = ~ eval(parse(text = col)), 
+              y = ~ eval(parse(text = weight)), 
+              type = 'bar', 
+              name = 'weight',
+              marker = list(color = 'yellow'),
+              hoverinfo = "text",
+              text = ~ paste(round(eval(parse(text = weight)),
+                                   rounding_digits), 
+                             ' (sum weight)')) %>%
+    
+    add_trace(x = ~ eval(parse(text = col)), 
+              y = ~ eval(parse(text = observed)), 
+              type = 'scatter', 
+              mode = 'lines', 
+              name = 'observed', 
+              yaxis = 'y2',
+              line = list(color = 'pink'),
+              hoverinfo = "text",
+              text = ~ paste(round(eval(parse(text = observed)),
+                                   rounding_digits), 
+                             '(ave obs)')) %>%
+    
+    layout(#title = paste0("<br>", col),
+           title = col,
+           xaxis = list(title = ""),
+           yaxis = list(side = 'right', 
+                        title = 'total weight', 
+                        showgrid = F, 
+                        zeroline = F),
+           yaxis2 = list(side = 'left', 
+                         overlaying = "y", 
+                         title = 'average observed', 
+                         showgrid = F, 
+                         zeroline = F))
+  
+  p
+  
+}
 
 
 
