e55fc7f7ee0240ed9614fc4e7b4a14d4d086961a
richardangell <richardangell37@gmail.com>
2017-10-20 20:52:44 +0100

    add reactive column select drop downs

--- R/shiny-examples/app.R
+++ R/shiny-examples/app.R
@@ -16,57 +16,115 @@ data_frame_objects <- function(objects_name = ls(envir = globalenv())) {
 
 
 
+dashboardHeader <- dashboardHeader(title = "ladybird-umbrella")
 
-
-
-
-ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Widgets", icon = icon("th"), tabName = "widgets"),
-      menuItem("Charts", icon = icon("bar-chart-o"), startExpanded = TRUE,
-               menuSubItem("Sub-item 1", tabName = "subitem1"),
-               menuSubItem("Sub-item 2", tabName = "subitem2")
-      )
-    ),
-    textOutput("res")
+dashboardSidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", 
+             tabName = "dashboard", 
+             icon = icon("dashboard")),
+    menuItem("Widgets", 
+             icon = icon("th"), 
+             tabName = "widgets"),
+    menuItem("Charts", 
+             icon = icon("bar-chart-o"), 
+             startExpanded = FALSE,
+             menuSubItem("Sub-item 1", tabName = "subitem1"),
+             menuSubItem("Sub-item 2", tabName = "subitem2"))
   ),
-  dashboardBody(
-    tabItems(
-      tabItem("dashboard", 
-              "Dashboard tab content", 
-              selectInput(inputId = "dataset",
-                          label = "Choose a data.frame object:",
-                          choices = c("please select", 
-                                      data_frame_objects())),
-              selectInput(inputId = "weights",
-                          label = "Choose a column:",
-                          choices = c("please select", 
-                                      textOutput("input_dataset_colnames")))),
-      tabItem("widgets", "Widgets tab content"),
-      tabItem("subitem1", "Sub-item 1 tab content"),
-      tabItem("subitem2", "Sub-item 2 tab content")
-    )
+  textOutput("res")
+)
+
+
+dashboardBody <- dashboardBody(
+  tabItems(
+    tabItem("dashboard", 
+            "Dashboard tab content", 
+            selectInput(inputId = "dataset",
+                        label = "Choose a data.frame object:",
+                        choices = c("please select", 
+                                    data_frame_objects())),
+            selectInput(inputId = "weights_col",
+                        label = "weights column (required)",
+                        choices = ""),
+            selectInput(inputId = "observed_col",
+                        label = "observed column (optional)",
+                        choices = ""),
+            selectInput(inputId = "pred1_col",
+                        label = "predictions column 1 (optional)",
+                        choices = ""),
+            selectInput(inputId = "pred2_col",
+                        label = "predictions column 2 (optional)",
+                        choices = ""),
+            actionButton("button", "An action button")),
+    tabItem("widgets", "Widgets tab content"),
+    tabItem("subitem1", "Sub-item 1 tab content"),
+    tabItem("subitem2", "Sub-item 2 tab content")
   )
 )
 
+
+ui <- dashboardPage(dashboardHeader,
+                    dashboardSidebar,
+                    dashboardBody,
+                    title = "ladybird-umbrella")
+
 server <- function(input, output, session) {
+  
+  session$onSessionEnded(stopApp)
   
   observe(print(input$dataset))
   
-  output$input_dataset_colnames <- reactive({
-    
+  observe(print(input$df_columns))
+  
+  observe(print(input$button))
+  
+  observe({
+  
     if (input$dataset == "please select") {
       
-      "no data.frame selected"
+      df_col_choices <- "please select data.frame"
       
     } else {
       
-      colnames(get(input$dataset, envir = globalenv()))
+      df_col_choices <- c("please select",
+                          colnames(get(input$dataset, envir = globalenv())))
       
-    }})
+    }
+    
+    updateSelectInput(session, 
+                      inputId = "weights_col",
+                      label = "weights column (required)",
+                      choices = df_col_choices)
+    
+    updateSelectInput(session, 
+                      inputId = "observed_col",
+                      label = "observed column (optional)",
+                      choices = df_col_choices)
+    
+    updateSelectInput(session, 
+                      inputId = "pred1_col",
+                      label = "predictions column 1 (optional)",
+                      choices = df_col_choices)
+    
+    updateSelectInput(session, 
+                      inputId = "pred2_col",
+                      label = "predictions column 2 (optional)",
+                      choices = df_col_choices)
+    
+  })
+  
+  #output$input_dataset_colnames <- reactive({
+  #  
+  #  if (input$dataset == "please select") {
+  #    
+  #    "no data.frame selected"
+  #    
+  #  } else {
+  #    
+  #    colnames(get(input$dataset, envir = globalenv()))
+  #    
+  #  }})
   
   
   
@@ -76,4 +134,11 @@ server <- function(input, output, session) {
   })
 }
 
+
+
+
+
 shinyApp(ui, server)
+
+
+
